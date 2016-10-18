1f65a177b2ac3ef7297a30aa1fcda5311c4322f1
Santiago Lozano <slozano95@gmail.com>
2016-10-18 12:51:44 -0500

    Added started file for en translation

---
+++ src/Installs/resources/lang/en/laraadmin.php
@@ -0,0 +1,72 @@
+<?php
+
+return array (
+  'dashboard' => 
+  array (
+    'subtitle' => 'Organization Overview',
+    'title' => 'Dashboard
+',
+  ),
+  'layouts' => 
+  array (
+    'partials' => 
+    array (
+      'file_manager' => 
+      array (
+        'select_file\'' => 'Select File',
+        'public_question' => 'Is Public?',
+        'drop_here' => 'Drop Here',
+      ),
+    ),
+    'partial' => 
+    array (
+      'notifs' => 
+      array (
+        'register' => 'Register',
+        'login' => 'Login',
+        'member_since' => 'Member Since',
+        'editor' => 'File Editor',
+        'modules' => 'Modules',
+        'menus' => 'Menus',
+        'configure' => 'Configure',
+        'backups' => 'Backups',
+        'signout' => 'Sign Out',
+        'profile' => 'Profile',
+      ),
+      'sidebar' => 
+      array (
+        'online' => 'Online',
+        'search_3dots' => 'Search ...',
+        'dashboard' => 'Dashboard',
+        'modules' => 'Modules',
+      ),
+    ),
+  ),
+  'stubs' => 
+  array (
+    'views' => 
+    array (
+      'add_button' => 'Add :model',
+      'contentheader_description' => ':model Listings',
+      'listing' => 'Listing',
+      'close_button' => 'Close',
+      'submit_button' => 'Submit',
+      'edit' => 'Edit',
+      'cancel_button' => 'Cancel',
+      'update_button' => 'Update',
+      'created_at' => 'Created At',
+      'updated_at' => 'Updated At',
+      'module' => 'Module',
+      'view' => 'View',
+    ),
+  ),
+  'modules' => 
+  array (
+    'Jojojo' => 'JOJOJ',
+    'Jojojos' => 'BLBALBAL',
+    'singular' => 
+    array (
+      'Jojojo' => 'Bla',
+    ),
+  ),
+);
